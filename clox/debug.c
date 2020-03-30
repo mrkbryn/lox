a0a35ef39f86d1b1ff0bfa387167d263e10d6dc5
Mark Bryan <mab539@cornell.edu>
2020-03-29 22:07:26 -0400

    disassemble constants and values

--- clox/debug.c
+++ clox/debug.c
@@ -21,13 +21,24 @@ static int simpleInstruction(const char* name, int offset) {
     return offset + 1;
 }
 
+static int constantInstruction(const char* name, Chunk* chunk, int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 2;
+}
+
 int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d ", offset);
     
+    // process the instruction and return the new offset
     uint8_t instruction = chunk->code[offset];
     switch (instruction) {
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
+        case OP_CONSTANT:
+            return constantInstruction("OP_CONSTANT", chunk, offset);
         default:
             printf("Unknown opcode %d\n", instruction);
             return offset + 1;
