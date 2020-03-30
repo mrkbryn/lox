68a6c60b53c3719c080039fd248f6f824a42b17a
Mark Bryan <mab539@cornell.edu>
2020-03-29 21:51:02 -0400

    fix semi

--- clox/value.c
+++ clox/value.c
@@ -19,7 +19,7 @@ void writeValueArray(ValueArray* array, Value value) {
     if (array->capacity < array->count + 1) {
         int oldCapacity = array->capacity;
         array->capacity = GROW_CAPACITY(oldCapacity);
-        array->values = GROW_ARRAY(array->values, Value, oldCapacity, array->capacity)
+        array->values = GROW_ARRAY(array->values, Value, oldCapacity, array->capacity);
     }
     
     array->values[array->count] = value;
