507d57a295dc27ad370da03a678a8c32808cb585
Mark Bryan <mab539@cornell.edu>
2020-03-29 20:49:03 -0400

    Initial Commit

---
+++ clox/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  clox
+//
+//  Created by Mark Bryan on 3/29/20.
+//  Copyright © 2020 Mark Bryan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
