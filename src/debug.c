d57315e388ecb43698c6487f801ccf5fa7eae0c4
Mark Bryan <mab539@cornell.edu>
2020-11-25 13:57:29 -0600

    Support while loops

--- src/debug.c
+++ src/debug.c
@@ -100,6 +100,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return simpleInstruction("OP_GREATER", offset);
         case OP_LESS:
             return simpleInstruction("OP_LESS", offset);
+        case OP_LOOP:
+            return jumpInstruction("OP_LOOP", -1, chunk, offset);
         default:
             printf("Unknown opcode %d\n", instruction);
             return offset + 1;
