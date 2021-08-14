709253e14879b900d1714cfaee86a1c3263d96c7
Mark Bryan <mab539@cornell.edu>
2021-08-14 18:46:43 -0400

    Starting rlox implementation

--- rlox/src/main.rs
+++ rlox/src/main.rs
@@ -1,3 +1,27 @@
+mod token_type;
+
+use std::env;
+use std::fs;
+
+fn run_file(path: &String) {
+    println!("Running file! {}", path);
+    let contents = fs::read_to_string(path)
+        .expect("Failed to read file");
+    println!("Contents: {}", contents);
+}
+
+fn run_prompt() {
+    println!("Running prompt!")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2 {
+        println!("Usage rlox [script]");
+        std::process::exit(64);
+    } else if args.len() == 2 {
+        run_file(&args[1]);
+    } else {
+        run_prompt();
+    }
 }
