6291be27542eeafd35544b55618075c77f744555
Mark Bryan <mab539@cornell.edu>
2021-08-21 23:41:26 -0400

    Add class to AST and add simple class parsing.

--- jlox/src/main/java/lox/Parser.java
+++ jlox/src/main/java/lox/Parser.java
@@ -30,8 +30,9 @@ public class Parser {
     }
 
     private Stmt declaration() {
-        // declaration -> funDecl | varDecl | statement ;
+        // declaration -> classDecl | funDecl | varDecl | statement ;
         try {
+            if (match(CLASS)) return classDeclaration();
             if (match(FUN)) return function("function");
             if (match(VAR)) return varDeclaration();
             return statement();
@@ -39,6 +40,21 @@ public class Parser {
             synchronize();
             return null;
         }
+    }
+
+    private Stmt classDeclaration() {
+        // classDecl -> "class" IDENTIFIER "{" function* "}" ;
+        Token name = consume(IDENTIFIER, "Expect class name.");
+        consume(LEFT_BRACE, "Expect '{' before class body.");
+
+        List<Stmt.Function> methods = new ArrayList<>();
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            methods.add(function("method"));
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after class body.");
+
+        return new Stmt.Class(name, methods);
     }
 
     private Stmt statement() {
