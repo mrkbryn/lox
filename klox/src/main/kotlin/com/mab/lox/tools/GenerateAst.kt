f7291d883f391f54b2c80bd7fd453801817bc9f2
Mark Bryan <mab539@cornell.edu>
2021-11-22 00:03:24 -0600

    Cleanup AST generator

--- klox/src/main/kotlin/com/mab/lox/tools/GenerateAst.kt
+++ klox/src/main/kotlin/com/mab/lox/tools/GenerateAst.kt
@@ -3,48 +3,66 @@ package com.mab.lox.tools
 import java.io.PrintWriter
 import kotlin.system.exitProcess
 
+class GenerateAst {
+    companion object {
+        const val basePackage = "com.mab.lox"
+    }
+}
+
 fun main(args: Array<String>) {
     println("Running generate_ast with args: $args")
     if (args.size != 1) {
-        System.err.println("Usage: generate_ast <output directory>")
+        System.err.println("Usage: generateAst <output directory>")
         exitProcess(64)
     }
 
     val outputDir = args[0]
 
-    defineAst(outputDir, "Expr", listOf(
-        "Assign     | val name: Token, val value: Expr",
-        "Binary     | val left: Expr, val operator: Token, val right: Expr",
-        "Call       | val callee: Expr, val paren: Token, val arguments: List<Expr>",
-        "Get        | val obj: Expr, val name: Token",
-        "Grouping   | val expression: Expr",
-        "Literal    | val value: Any?",
-        "Logical    | val left: Expr, val operator: Token, val right: Expr",
-        "Set        | val obj: Expr, val name: Token, val value: Expr",
-        "Super      | val keyword: Token, val method: Token",
-        "This       | val keyword: Token",
-        "Variable   | val name: Token",
-        "Unary      | val operator: Token, val right: Expr",
-    ))
+    defineAst(
+        outputDir = outputDir,
+        baseName = "Expr",
+        types = listOf(
+            "Assign     | val name: Token, val value: Expr",
+            "Binary     | val left: Expr, val operator: Token, val right: Expr",
+            "Call       | val callee: Expr, val paren: Token, val arguments: List<Expr>",
+            "Get        | val obj: Expr, val name: Token",
+            "Grouping   | val expression: Expr",
+            "Literal    | val value: Any?",
+            "Logical    | val left: Expr, val operator: Token, val right: Expr",
+            "Set        | val obj: Expr, val name: Token, val value: Expr",
+            "Super      | val keyword: Token, val method: Token",
+            "This       | val keyword: Token",
+            "Variable   | val name: Token",
+            "Unary      | val operator: Token, val right: Expr",
+        )
+    )
 
-    defineAst(outputDir, "Stmt", listOf(
-        "Block      | val statements: List<Stmt>",
-        "Class      | val name: Token, val superclass: Expr.Variable?, val methods: List<Stmt.Function>",
-        "Expression | val expression: Expr",
-        "Function   | val name: Token, val params: List<Token>, val body: List<Stmt>",
-        "If         | val condition: Expr, val thenBranch: Stmt, val elseBranch: Stmt?",
-        "Var        | val name: Token, val initializer: Expr?",
-        "Print      | val expression: Expr",
-        "Return     | val keyword: Token, val value: Expr?",
-        "While      | val condition: Expr, val body: Stmt",
-    ))
+    defineAst(
+        outputDir = outputDir,
+        baseName = "Stmt",
+        types = listOf(
+            "Block      | val statements: List<Stmt>",
+            "Class      | val name: Token, val superclass: Expr.Variable?, val methods: List<Stmt.Function>",
+            "Expression | val expression: Expr",
+            "Function   | val name: Token, val params: List<Token>, val body: List<Stmt>",
+            "If         | val condition: Expr, val thenBranch: Stmt, val elseBranch: Stmt?",
+            "Var        | val name: Token, val initializer: Expr?",
+            "Print      | val expression: Expr",
+            "Return     | val keyword: Token, val value: Expr?",
+            "While      | val condition: Expr, val body: Stmt",
+        )
+    )
 }
 
-fun defineAst(outputDir: String, baseName: String, types: List<String>) {
+fun defineAst(
+    outputDir: String,
+    baseName: String,
+    types: List<String>
+) {
     val path = "$outputDir/${baseName}.kt"
     val writer = PrintWriter(path, "UTF-8")
 
-    writer.println("package com.mab.lox")
+    writer.println("package ${GenerateAst.basePackage}")
     writer.println()
     writer.println("abstract class $baseName {")
 
@@ -62,7 +80,11 @@ fun defineAst(outputDir: String, baseName: String, types: List<String>) {
     writer.close()
 }
 
-fun defineVisitor(writer: PrintWriter, baseName: String, types: List<String>) {
+fun defineVisitor(
+    writer: PrintWriter,
+    baseName: String,
+    types: List<String>
+) {
     writer.println("    interface Visitor<R> {")
     types.forEach {
         val typeName = it.split("|")[0].trim()
@@ -72,7 +94,12 @@ fun defineVisitor(writer: PrintWriter, baseName: String, types: List<String>) {
     writer.println()
 }
 
-fun defineType(writer: PrintWriter, baseName: String, className: String, fieldList: String) {
+fun defineType(
+    writer: PrintWriter,
+    baseName: String,
+    className: String,
+    fieldList: String
+) {
     writer.println("    class $className($fieldList) : $baseName() {")
     writer.println("        override fun <R> accept(visitor: Visitor<R>): R {")
     writer.println("            return visitor.visit$className$baseName(this)")
