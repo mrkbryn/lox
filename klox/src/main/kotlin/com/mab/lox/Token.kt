feb6899cc237984f6b87c3d898eb83cff15a1f00
Mark Bryan <mab539@cornell.edu>
2021-11-21 23:42:34 -0600

    Final rename of files.

--- klox/src/main/kotlin/com/mab/lox/Token.kt
+++ klox/src/main/kotlin/com/mab/lox/Token.kt
@@ -18,3 +18,14 @@ enum class TokenType {
 
     EOF
 }
+
+class Token(
+    val type: TokenType,
+    val lexeme: String,
+    val literal: Any?,
+    val line: Int
+) {
+    override fun toString(): String {
+        return "$type $lexeme $literal"
+    }
+}
